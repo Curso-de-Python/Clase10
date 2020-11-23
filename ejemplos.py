71a23e9dff237ed2076a5cc9be2fe6274c0e988a
adrianababilonia22 <72059895+adrianababilonia22@users.noreply.github.com>
2020-11-23 11:18:53 -0500

    Create ejemplos.py

---
+++ ejemplos.py
@@ -0,0 +1,25 @@
+# FUNCIONES DEL MÓDULO MATH
+from math import ceil, floor, trunc
+
+x = 1.4
+y = 2.6
+
+print('ceil (entero mayor):')
+print(ceil(x), ceil(y))
+print(ceil(-x), ceil(-y))
+print('floor (entero menor):')
+print(floor(x), floor(y))
+print(floor(-x), floor(-y))
+print('trunc (truncado a entero):')
+print(trunc(x), trunc(y))
+print(trunc(-x), trunc(-y))
+
+# REPASO DE CADENAS
+palabra = 'por'
+print(len(palabra))
+
+vacio = ''
+print(len(vacio))
+
+yo_soy = 'I\'m'
+print(len(yo_soy))
